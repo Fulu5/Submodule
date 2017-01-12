d7e70bcf054c3ab9d452c416b5d2551c72b8d64c
gxcao <1120643432@qq.com>
2017-01-12 11:37:29 +0800

    Add project woth submodule

---
+++ SwiftGitSubModule/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftGitSubModule
+//
+//  Created by Guanxiong Cao  on 12/01/2017.
+//  Copyright © 2017 Guanxiong Cao . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("module")
+    }
+}
+
